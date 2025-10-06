c414c494808022844630f42ac77cc219e6cac8ad
Abdelrahman Elshareif <Abdelrahmanelshareif@gmail.com>
2025-10-06 21:06:42 +0300

    fix routes api.php

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,4 +1,8 @@
 <?php
+use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\Auth\PasswordResetController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\CategoryController;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
